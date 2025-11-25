aab831cc8b0cdaebf900549b8014815483deed08
Andreas Haug <andreas.haug@consto.no>
2025-11-25 20:47:34 +0100

    Add iOS and Android platforms with Capacitor config

---
+++ android/app/src/main/java/app/kvittr/MainActivity.java
@@ -0,0 +1,5 @@
+package app.kvittr;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.kvittr',
+  appName: 'Kvittr',
+  webDir: 'dist'
+};
+
+export default config;
