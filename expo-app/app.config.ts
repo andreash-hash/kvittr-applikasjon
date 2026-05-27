70bc220cc1a2c0c2071f473c63a0a58cec8b1c2f
andreash-hash <andreashaug@outlook.com>
2026-05-27 20:53:28 +0000

    chore: bump build number to 106 (v1.5.7)

--- expo-app/app.config.ts
+++ expo-app/app.config.ts
@@ -15,7 +15,7 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'app.kvittr',
-      buildNumber: '105',
+      buildNumber: '106',
       backgroundColor: '#0F1729',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
