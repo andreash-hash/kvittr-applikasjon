1060aadf4e264b4d0bcf835563342b3c11c21932
andreash-hash <andreashaug@outlook.com>
2026-05-27 07:06:19 +0000

    chore: bump version to 1.5.5 (build 101)

--- expo-app/app.config.ts
+++ expo-app/app.config.ts
@@ -4,7 +4,7 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
     ...config,
     name: 'Kvittr',
     slug: 'kvittr-ccode',
-    version: '1.5.4',
+    version: '1.5.5',
     scheme: 'kvittr',
     orientation: 'portrait',
     icon: './assets/icon.png',
@@ -17,7 +17,7 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'app.kvittr',
-      buildNumber: '100',
+      buildNumber: '101',
       backgroundColor: '#0F1729',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
