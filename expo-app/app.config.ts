d3f21805a912f834255f4902508ad67de0667f1e
andreash-hash <andreashaug@outlook.com>
2026-05-29 21:49:15 +0200

    chore: bump buildNumber to 115 [paywall fix]

--- expo-app/app.config.ts
+++ expo-app/app.config.ts
@@ -15,7 +15,7 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'app.kvittr',
-      buildNumber: '114',
+      buildNumber: '115',
       backgroundColor: '#0F1729',
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
